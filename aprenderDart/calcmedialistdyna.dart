76d84a67ac3bdf1dff14a5f73732d5da8fb17cdd
sam <pflegermendes@gmail.com>
2022-09-09 20:52:35 -0300

    atualizacoes

---
+++ aprenderDart/calcmedialistdyna.dart
@@ -0,0 +1,9 @@
+void main () {
+  List <dynamic> infoA1 = ["Joana", 6.0, "LA"];
+  List <dynamic> infoA2 = ["SO", "Joao", 8.60];
+  List <dynamic> infoA3 = [10.0, "Amélia", "KO"];
+
+  double media = (infoA1 [1] + infoA2 [2] + infoA3 [0]) /3;
+
+  print ("A média dos três estudantes é: $media");
+}
