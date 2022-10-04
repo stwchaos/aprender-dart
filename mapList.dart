8d421fffacc9a6d81b013a1e293b74b189088dd9
sam <pflegermendes@gmail.com>
2022-10-04 12:03:10 -0300

    new commit

--- mapList.dart
+++ mapList.dart
@@ -8,5 +8,18 @@ void main(List<String> args) {
   print('--------Key 0---------\n${Musicas[0]}\n\n'
       '--------Key 1---------\n${Musicas[1]}\n\n'
       '--------Key 2---------\n${Musicas[2]}\n\n\n'
-      '--------All list---------\n$Musicas');
+      '--------All list---------\n$Musicas\n\n\n--------------------\n\n');
+
+//console -> forEach
+  Musicas.forEach((key, value) {
+    print("""
+      A chave: $key
+        Contêm: ${value}
+    """);
+  });
+
+// Acrescentar novas músicas caso o Map for nulo.
+  Map<int, List<String>>? teste = null;
+
+  var newMusicas = {...Musicas, ...?teste};
 }
